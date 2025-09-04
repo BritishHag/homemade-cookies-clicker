8e04edfeb6b8b5ef58a8a5793e8cb6e6bb8e0902
BritishHag <43rfontenot@crschools.us>
2025-09-04 14:09:47 -0500

    qol

--- index/index.js
+++ index/index.js
@@ -2,9 +2,9 @@ const number = document.getElementById('button')
 const display = document.getElementById('display')
 const name = document.getElementById('name')
 
+
 let score = 0;
 let plusValue = 1;
-let secretnameActivation
 
 number.addEventListener('click', function() {
     score += plusValue;
